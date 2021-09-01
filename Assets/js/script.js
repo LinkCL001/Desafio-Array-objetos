e301b92e1f077f55bb6d5664e777092b1ea6d1da
JuanQuezada <ju.quezada.c@gmail.com>
2021-09-01 14:46:30 -0400

    commit

---
+++ Assets/js/script.js
@@ -0,0 +1,29 @@
+let especialistas = [""]
+let pacientes = [""]
+let prevision = ["FONASA", "ISAPRE"]
+let listados = ["Traumatología","Radiología","Dental"]
+
+let radiologia = {
+    
+        paciente : "FRANCISCA ROJAS",
+        rut : "9878782-1",
+        hora : "11:00",
+        prevision : "FONASA",
+        especialista : "IGNACIO SCHULZ",
+    },
+    
+        paciente : "PAMELA ESTRADA",
+        rut : "15345241-3",
+        hora : "11:00",
+        prevision : "ISAPRE",
+        especialista : "FEDERICO SUBERCASEAUX",
+    
+    },
+
+
+
+
+
+
+
+var texto = ""
